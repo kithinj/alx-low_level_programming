46a8830f205f20772058b9b79e95126e16f8e437
kithinj <kithinjifestus05@gmail.com>
2023-06-21 06:06:30 -0700

    update

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print all alphabet in lowercase.
+ *
+ * Return: Always 0
+ */
+
+void print_alphabet(void)
+{
+	char alpha = 'a';
+
+	while (alpha <= 'z')
+	{
+		_putchar (alpha);
+		alpha++;
+	}
+	_putchar ('\n');
+}
